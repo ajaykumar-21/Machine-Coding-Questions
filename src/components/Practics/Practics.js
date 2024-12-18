c9a7bc0e04748c1b17316a7a6be660eb41634ee2
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-12-18 17:23:25 +0530

    practics for infinite scroll

--- src/components/Practics/Practics.js
+++ src/components/Practics/Practics.js
@@ -1,55 +1,35 @@
-import React, { Children, useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Practics.module.css";
 
-const CustomSwitch = ({ children, value }) => {
-  let cases = [];
-  let defaults = [];
-  children.forEach((val) => {
-    if (val.type.name === "CustomCase") {
-      if (typeof val.props.value === "function") {
-        if (val.props.value(value)) {
-          cases.push(val);
-          console.log("function");
-        }
-      } else if (val.props.value === value) {
-        console.log(val.props.value);
-        cases.push(val);
-      }
-    } else if (val.type.name === "DefaultCase") {
-      defaults.push(val);
-    }
-  });
-
-  if (cases.length > 0) {
-    return cases;
-  } else {
-    return defaults;
-  }
-};
-
-const CustomCase = ({ children }) => {
-  return <div>{children}</div>;
-};
-
-const DefaultCase = ({ children }) => {
-  return <div>{children}</div>;
-};
-
 function Practics() {
+  const [count, setCount] = useState(50);
+
+  const onScroll = () => {
+    console.log(
+      window.innerHeight + window.scrollY,
+      window.document.body.offsetHeight - 50
+    );
+    if (
+      window.innerHeight + window.scrollY >=
+      window.document.body.offsetHeight - 50
+    ) {
+      setCount(count + 50);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+  }, [count]);
+
+  const element = [];
+  for (let i = 1; i <= count; i++) {
+    element.push(<div>{i}</div>);
+  }
+
   return (
-    <>
-      <CustomSwitch value="30">
-        <CustomCase value={(e) => e < 10}>
-          <div>Hello less than 10</div>
-        </CustomCase>
-        <CustomCase value="20">Hello 20</CustomCase>
-        <CustomCase value="30">Hello 30</CustomCase>
-        <CustomCase value="10">
-          <div>Hello 10</div>
-        </CustomCase>
-        <DefaultCase>Hello 40</DefaultCase>
-      </CustomSwitch>
-    </>
+    <div>
+      <div>{element}</div>
+    </div>
   );
 }
 
