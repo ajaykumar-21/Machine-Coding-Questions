ca0cdf236d61f45ffe9d06593bc866d1886ef982
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-03-13 15:32:23 +0530

    practics for auto suggestion

--- src/components/Practics/Practics.js
+++ src/components/Practics/Practics.js
@@ -1,74 +1,66 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./Practics.module.css";
 
-const images = [
-  {
-    image_url:
-      "https://img.freepik.com/free-photo/young-female-jacket-shorts-presenting-comparing-something-looking-confident-front-view_176474-37521.jpg?w=1800&t=st=1693037944~exp=1693038544~hmac=97e967909706f9b73b4b47d521acf54806f4b9b3efab6196bc8a69f07efff554",
-    caption: "Image 1",
-  },
-  {
-    image_url:
-      "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
-    caption: "Image 2",
-  },
-  {
-    image_url:
-      "https://img.freepik.com/free-photo/young-lady-shirt-jacket-making-scales-gesture-looking-cheerful-front-view_176474-85195.jpg?t=st=1693037931~exp=1693038531~hmac=2f83b6689538e4056912c96f448163e9ef10998f48f671b7e50279f81611fbe6",
-    caption: "Image 3",
-  },
-  {
-    image_url:
-      "https://img.freepik.com/free-photo/girl-wide-opening-hands-giving-explanation-high-quality-photo_144627-60466.jpg?w=1800&t=st=1693038021~exp=1693038621~hmac=d4520cd86b2aea3e5dda765ede05bb53d70e18a574756d0f41a6806fe325d26d",
-    caption: "Image 4",
-  },
+const mockData = [
+  { id: 1, name: "Leanne Graham" },
+  { id: 2, name: "Ervin Howell" },
+  { id: 3, name: "Clementine Bauch" },
 ];
 
 function Practics() {
-  const [active, setActive] = useState(0);
+  const inputRef = useRef(null);
+  const suggestionRef = useRef(null);
+  const [query, setQery] = useState("");
+  const [list, setlist] = useState([]);
+  const [suggestionAreaVisibility, setSuggestionAreaVisibility] =
+    useState(false);
 
-  const handleNext = () => {
-    setActive(active !== images.length - 1 ? active + 1 : 0);
-  };
+  useEffect(() => {
+    if (query.length > 0) {
+      setSuggestionAreaVisibility(true);
+      const nameData = mockData.map((e) => e.name);
+      const filtered = nameData.filter((data) =>
+        data.toLowerCase().includes(query.toLowerCase())
+      );
+      setlist(filtered);
+    } else {
+      setlist([]);
+    }
+  }, [query]);
 
-  const handlePrev = () => {
-    setActive(active !== 0 ? active - 1 : images.length - 1);
-  };
+  useEffect(() => {
+    window.addEventListener("click", (e) => {
+      console.log(e.target, inputRef.current, suggestionRef.current);
+      if (e.target !== inputRef.current && e.target !== suggestionRef.current) {
+        setSuggestionAreaVisibility(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("click", () => {});
+    };
+  });
+
   return (
     <div className={style.container}>
-      <div className={style.wrapper}>
-        <div className={style.slideContainer}>
-          {images &&
-            images.map((image, idx) => (
-              <div
-                key={idx}
-                className={`${style.slideShow} ${
-                  active === idx ? style.active : ""
-                }`}
-              >
-                <img
-                  src={image.image_url}
-                  alt={image.caption}
-                  width="100%"
-                  height="100%"
-                />
-              </div>
-            ))}
-          <div className={style.buttonWrapper}>
-            <div onClick={handlePrev}>{"<"}</div>
-            <div onClick={handleNext}>{">"}</div>
+      <div>
+        <h2>Auto Search Suggestions</h2>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQery(e.target.value)}
+          ref={inputRef}
+        />
+        {suggestionAreaVisibility && (
+          <div className={style.suggestonArea} ref={suggestionRef}>
+            {list &&
+              list.map((e, i) => (
+                <div key={i} onClick={() => setQery(e)}>
+                  {e}
+                </div>
+              ))}
           </div>
-        </div>
-        <div className={style.dotWrapper}>
-          {images.map((e, idx) => (
-            <div
-              className={`${style.dot} ${
-                active === idx ? style.active : ""
-              }`}
-              key={e.caption}
-            ></div>
-          ))}
-        </div>
+        )}
       </div>
     </div>
   );
