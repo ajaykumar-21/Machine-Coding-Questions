aa5720bac90fef6badba0cfff14becb0ec1d86ff
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-03-18 15:23:15 +0530

    practics for Feature Flag component

--- src/components/Practics/Practics.js
+++ src/components/Practics/Practics.js
@@ -1,54 +1,50 @@
-import React, { act, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./Practics.module.css";
 
-const light = {
-  red: "red",
-  yellow: "yellow",
-  green: "green",
-};
-function Practics() {
-  const [active, setActive] = useState(light.red);
+const FeatureFlags = React.createContext({});
 
-  useEffect(() => {
-    if (active === light.red) {
-      setTimeout(() => {
-        setActive(light.green);
-      }, 4000);
-    } else if (active === light.green) {
-      setTimeout(() => {
-        setActive(light.yellow);
-      }, 5000);
-    } else {
-      setTimeout(() => {
-        setActive(light.red);
-      }, 3000);
-    }
-  }, [active]);
+function FlagProvider({ children }) {
+  const [features, setFeatures] = useState({
+    isGooglePayEnabled: true,
+    isePhonePayEnabled: false,
+  });
 
   return (
     <>
-      <div class className={style.wrapper}>
-        <div
-          className={`${style.red} ${style.traffic}`}
-          style={active !== "red" ? { opacity: 0.5 } : null}
-        >
-          A
-        </div>
-        <div
-          className={`${style.green} ${style.traffic}`}
-          style={active !== "green" ? { opacity: 0.5 } : null}
-        >
-          B
-        </div>
-        <div
-          className={`${style.yellow} ${style.traffic}`}
-          style={active !== "yellow" ? { opacity: 0.5 } : null}
-        >
-          C
-        </div>
-      </div>
+      <FeatureFlags.Provider value={{ features }}>
+        {children}
+      </FeatureFlags.Provider>
     </>
   );
 }
+
+const PaymentFlag = () => {
+  return (
+    <>
+      <Feature feature="isGooglePayEnabled">Google</Feature>
+      <Feature feature="isePhonePayEnabled">Phone pe</Feature>
+    </>
+  );
+};
+
+const Feature = ({ feature, children }) => {
+  console.log(children, feature);
+  const { features } = React.useContext(FeatureFlags);
+  return features[feature] === true ? children : null;
+  // return (
+  //   <>
+  //     <h1>{features.isGooglePayEnabled ? "Google" : null}</h1>
+  //     <h1>{features.isePhonePayEnabled ? "Phone Pe" : null}</h1>
+  //   </>
+  // );
+};
+
+const Practics = () => {
+  return (
+    <FlagProvider>
+      <PaymentFlag />
+    </FlagProvider>
+  );
+};
 
 export default Practics;
