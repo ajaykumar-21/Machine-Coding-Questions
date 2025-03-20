3a5d5db908189e0ebe6c412ed4c04bae0bc46c6e
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-03-20 16:17:19 +0530

    practics for infinite scroll bar component

--- src/components/Practics/Practics.js
+++ src/components/Practics/Practics.js
@@ -1,50 +1,31 @@
 import React, { useEffect, useRef, useState } from "react";
 import style from "./Practics.module.css";
 
-const FeatureFlags = React.createContext({});
-
-function FlagProvider({ children }) {
-  const [features, setFeatures] = useState({
-    isGooglePayEnabled: true,
-    isePhonePayEnabled: false,
-  });
-
-  return (
-    <>
-      <FeatureFlags.Provider value={{ features }}>
-        {children}
-      </FeatureFlags.Provider>
-    </>
-  );
-}
-
-const PaymentFlag = () => {
-  return (
-    <>
-      <Feature feature="isGooglePayEnabled">Google</Feature>
-      <Feature feature="isePhonePayEnabled">Phone pe</Feature>
-    </>
-  );
-};
-
-const Feature = ({ feature, children }) => {
-  console.log(children, feature);
-  const { features } = React.useContext(FeatureFlags);
-  return features[feature] === true ? children : null;
-  // return (
-  //   <>
-  //     <h1>{features.isGooglePayEnabled ? "Google" : null}</h1>
-  //     <h1>{features.isePhonePayEnabled ? "Phone Pe" : null}</h1>
-  //   </>
-  // );
-};
-
 const Practics = () => {
-  return (
-    <FlagProvider>
-      <PaymentFlag />
-    </FlagProvider>
-  );
+  const [count, setCount] = useState(50);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (
+        Math.ceil(window.innerHeight + window.scrollY) >=
+        document.body.offsetHeight - 10
+      ) {
+        setCount(count + 50);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [count]);
+
+  const elements = [];
+  for (let i = 0; i <= count; i++) {
+    elements.push(<div key={i}>{i}</div>);
+  }
+  return elements;
 };
 
 export default Practics;
