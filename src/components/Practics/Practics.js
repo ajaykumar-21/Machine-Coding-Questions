aa609e0e7463cab2f7de777835e01af4eec83e24
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-03-21 18:54:49 +0530

    practics for pagination component

--- src/components/Practics/Practics.js
+++ src/components/Practics/Practics.js
@@ -2,28 +2,71 @@ import React, { useEffect, useRef, useState } from "react";
 import style from "./Practics.module.css";
 
 const Practics = () => {
-  const [show, setShow] = useState(false);
+  const [employees, setEmployees] = useState(null);
+  const [totalPage, setTotalPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetch(
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const pages = Math.ceil(data.length / 10);
+        setTotalPage(pages);
+        setEmployees(data);
+      })
+      .catch((error) => {
+        console.log("Error while fetching the data", error);
+        alert("Somthing went wrong");
+      });
+  }, []);
+
+  const handlePrev = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const startIndex = (currentPage - 1) * 10;
+  const lastIndex = Math.min(startIndex + 10, employees.length);
+  const pageData = employees.slice(startIndex, lastIndex);
 
   return (
-    <div
-      className={`${style.modalContainer} ${
-        show ? style.backgroundColor : ""
-      }`}
-    >
-      <button onClick={() => setShow(true)}>
-        {show ? "Hide" : "Show"} Modal
-      </button>
-      {show && (
-        <div className={`${style.container} ${show ? style.active : ""}`}>
-          <div className={style.wrapper}>
-            <div>Modal</div>
-            <div className={style.close} onClick={() => setShow(false)}>
-              X
-            </div>
-          </div>
-          <div className={style.component}>Modal Component</div>
-        </div>
-      )}
+    <div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pageData &&
+              pageData.map((employee) => (
+                <tr key={employee.id}>
+                  <td>{employee.id}</td>
+                  <td>{employee.name}</td>
+                  <td>{employee.email}</td>
+                  <td>{employee.role}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <button onClick={handlePrev} disabled={currentPage === 1}>
+          Prev
+        </button>
+        <button onClick={handleNext} disabled={currentPage === totalPage}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
