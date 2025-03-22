e58b6b7500a448f1618c621855dd243675a1802d
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-03-22 14:38:26 +0530

    practics for user table  component

--- src/components/Practics/Practics.js
+++ src/components/Practics/Practics.js
@@ -2,72 +2,71 @@ import React, { useEffect, useRef, useState } from "react";
 import style from "./Practics.module.css";
 
 const Practics = () => {
-  const [employees, setEmployees] = useState(null);
-  const [totalPage, setTotalPage] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [user, setUser] = useState({
+    name: "",
+    gender: "",
+    mobile: "",
+  });
+  const [userList, setUserList] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const pages = Math.ceil(data.length / 10);
-        setTotalPage(pages);
-        setEmployees(data);
-      })
-      .catch((error) => {
-        console.log("Error while fetching the data", error);
-        alert("Somthing went wrong");
-      });
-  }, []);
-
-  const handlePrev = () => {
-    setCurrentPage(currentPage - 1);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUserList((prevList) => [...prevList, user]);
   };
 
-  const handleNext = () => {
-    setCurrentPage(currentPage + 1);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
-
-  const startIndex = (currentPage - 1) * 10;
-  const lastIndex = Math.min(startIndex + 10, employees.length);
-  const pageData = employees.slice(startIndex, lastIndex);
-
+  console.log(userList);
   return (
-    <div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          placeholder="Enter your full name"
+        />{" "}
+        <input
+          type="text"
+          name="gender"
+          onChange={handleChange}
+          placeholder="Enter your Gender"
+        />{" "}
+        <input
+          type="number"
+          name="mobile"
+          onChange={handleChange}
+          placeholder="Enter your mobile number"
+        />
+        <button type="submit">submit</button>
+      </form>
       <div>
         <table>
           <thead>
             <tr>
-              <th>Id</th>
               <th>Name</th>
-              <th>Email</th>
-              <th>Role</th>
+              <th>Gender</th>
+              <th>Mobile</th>
             </tr>
           </thead>
           <tbody>
-            {pageData &&
-              pageData.map((employee) => (
-                <tr key={employee.id}>
-                  <td>{employee.id}</td>
-                  <td>{employee.name}</td>
-                  <td>{employee.email}</td>
-                  <td>{employee.role}</td>
+            {userList &&
+              userList.map((user) => (
+                <tr key={user.name}>
+                  <td>{user.name}</td>
+                  <td>{user.gender}</td>
+                  <td>{user.mobile}</td>
                 </tr>
               ))}
           </tbody>
         </table>
       </div>
-      <div>
-        <button onClick={handlePrev} disabled={currentPage === 1}>
-          Prev
-        </button>
-        <button onClick={handleNext} disabled={currentPage === totalPage}>
-          Next
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
