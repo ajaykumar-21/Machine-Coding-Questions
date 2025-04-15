c8f020bd4086b3a60bc492d119bdfa09af177882
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-04-15 16:50:43 +0530

    add necessay functionality in otp component

--- src/components/Otp/Otp.js
+++ src/components/Otp/Otp.js
@@ -1,29 +1,66 @@
 import React, { useRef, useState } from "react";
 import styles from "./Otp.module.css";
 
-const boxes = Array(6).fill(0);
+const boxes = Array(6).fill("");
 function Otp() {
-  const [value, setVlaue] = useState("");
-  const ref = useRef();
-  console.log(ref);
+  const [inputField, setInputField] = useState(boxes);
+  const ref = useRef([]);
 
-  const onHandleChange = (e, index) => {
-    console.log(e.key);
+  const handleKeyDown = (e, index) => {
+    const { key } = e;
+    const copyValue = [...inputField];
+
+    if (key === "ArrowRight") {
+      if (index + 1 < inputField.length) {
+        ref.current[index + 1].focus();
+        return;
+      }
+    }
+
+    if (key === "ArrowLeft") {
+      if (index !== 0) {
+        ref.current[index - 1].focus();
+        return;
+      }
+    }
+
+    if (key === "Backspace") {
+      if (index !== 0) {
+        copyValue[index] = "";
+        setInputField(copyValue);
+        ref.current[index - 1].focus();
+        return;
+      } else {
+        copyValue[index] = "";
+        setInputField(copyValue);
+      }
+    }
+
+    if (isNaN(key)) {
+      return;
+    }
+
+    if (index + 1 < inputField.length) {
+      ref.current[index + 1].focus();
+    }
+    copyValue[index] = key;
+    setInputField(copyValue);
   };
 
-  
   return (
     <div className={styles.container}>
-      {boxes.map((_, index) => (
-        <input
-          key={index}
-          className={styles.otpBox}
-          type="text"
-          value={value}
-          onChange={(e) => onHandleChange(e, index)}
-          ref={ref.current}
-        />
-      ))}
+      {inputField &&
+        inputField.map((value, index) => (
+          <input
+            key={index}
+            className={styles.otpBox}
+            type="text"
+            value={value}
+            //   onChange={(e) => onHandleChange(e)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+            ref={(currentInput) => (ref.current[index] = currentInput)}
+          />
+        ))}
     </div>
   );
 }
