08cd1e53a282acad85e6426d0a950d747ec8dee1
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-04-16 15:45:16 +0530

    add tab functionality to change tab

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import data from "./data.json";
+import styles from "./Tab.module.css";
 
 function Tab() {
+  const [currentTab, setCurrentTab] = useState(0);
   return (
-    <div>Tab</div>
-  )
+    <div className={styles.tabContainer}>
+      <div className={styles.btnContainer}>
+        {data.map((btn, idx) => (
+          <button
+            className={`${styles.tabBtn} ${
+              currentTab === idx ? styles.active : ""
+            }`}
+            onClick={() => setCurrentTab(idx)}
+          >
+            {btn.button}
+          </button>
+        ))}
+      </div>
+      <div className={styles.content}>{data[currentTab].content}</div>
+    </div>
+  );
 }
 
-export default Tab
+export default Tab;
