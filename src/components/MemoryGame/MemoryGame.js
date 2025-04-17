bbd015d1ebfe930bc5e291e8c047fd811d1466a6
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-04-17 14:37:46 +0530

    create a grid for memory game component

--- src/components/MemoryGame/MemoryGame.js
+++ src/components/MemoryGame/MemoryGame.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./MemoryGame.module.css";
 
 function MemoryGame() {
+  const [cards, setCards] = useState(generateGrid());
+  console.log(cards);
   return (
-    <div>MemoryGame</div>
-  )
+    <div className={styles.gridContainer}>
+      {cards.map(({ id, number, isFlipped }) => (
+        <button key={id} className={styles.cards}>
+          {isFlipped ? number : "?"}
+        </button>
+      ))}
+    </div>
+  );
 }
 
-export default MemoryGame
+export default MemoryGame;
+
+function generateGrid() {
+  const arr = Array.from({ length: 18 }, (_, index) => index + 1);
+  const grid = [...arr, ...arr].sort(() => Math.random() - 0.5);
+  const cards = grid.map((item, index) => {
+    return {
+      id: index,
+      number: item,
+      isFlipped: false,
+    };
+  });
+  return cards;
+}
