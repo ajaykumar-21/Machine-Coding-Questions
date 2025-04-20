e487bca312eb31615b3868a728eb646bc5f001dd
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-04-20 13:55:55 +0530

    create drag and drop component

---
+++ src/components/DragAndDrop/DragAndDrop.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const initialData = {
+  Todo: [
+    "Design UI Makeup",
+    "Set up project repository",
+    "Write unit test",
+    "Integrate payment gateway",
+  ],
+  "In Progress": [
+    "Develop authentication flow",
+    "Implement New features",
+    "Connecting to the main gate",
+  ],
+  Completed: [
+    "Set up CI/CD pipeline",
+    "Conduct code reviews",
+    "Deploy initial cersion to staging",
+  ],
+};
+
+function DragAndDrop() {
+  return <div>DragAndDrop</div>;
+}
+
+export default DragAndDrop;
